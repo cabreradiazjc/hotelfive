dbd7e76836958380246cb56652fce72567b670b6
cabreradiazjc <cabreradiaz.jc@gmail.com>
2016-06-02 07:38:53 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class=" ext-strict"><head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login de Usuario</title>
+        <link rel="stylesheet" type="text/css" href="ext/resources/css/ext-all.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <script type="text/javascript" src="ext/adapter/ext/ext-base.js"></script>
+        <script type="text/javascript" src="ext/ext-all.js"></script> 
+        <script type="text/javascript" src="js/login.js"></script>
+        <script type="text/javascript" src="ext/src/locale/ext-lang-es.js"></script>
+        <script  type="text/javascript" src="ext/ux/ext.util.md5.js"></script> 
+        <!--link rel="stylesheet" type="text/css" href="ext/resources/css/tema.css"-->
+    </head>
+    <body>
+</body>
+</html>
+
